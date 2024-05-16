0a8dd4fde45caa93d94ee92c24e4d24427635708
xstefank <xstefank122@gmail.com>
2024-05-16 22:08:06 +0200

    GeeCon Krakow 2024

--- src/main/java/org/acme/Virtual.java
+++ src/main/java/org/acme/Virtual.java
@@ -3,11 +3,84 @@ package org.acme;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Virtual {
 
-    void main() throws Exception {
+    static ThreadLocal<Expensive> threadLocal = ThreadLocal.withInitial(Expensive::new);
 
+    record Expensive(UUID uuid) {
+        Expensive() {
+            this(UUID.randomUUID());
+        }
+
+        @Override
+        public String toString() {
+            return "Expensive{" +
+                "uuid=" + uuid +
+                '}';
+        }
+    }
+
+    void main() throws Exception {
+//        Thread vta = Thread.ofVirtual().start(() -> {
+//            log("Thread A start");
+//            sleep(100);
+//            log("Thread A end");
+//        });
+//        Thread vtb = Thread.ofVirtual().start(() -> {
+//            log("Thread B start");
+//            sleep(100);
+//            log("Thread B end");
+//        });
+//
+//        vta.join();
+//        vtb.join();
+
+        ThreadFactory factory = Thread.ofVirtual().name("geecon-", 0).factory();
+        ExecutorService executor = Executors.newThreadPerTaskExecutor(factory);
+
+        Future<?> f1 = executor.submit(() -> {
+            log(threadLocal.get().toString());
+            pinLock();
+        });
+        Future<?> f2 = executor.submit(() -> {
+            log(threadLocal.get().toString());
+            log("Thread B start");
+            sleep(100);
+            log("Thread B end");
+        });
+
+        f1.get();
+        f2.get();
+    }
+
+    static ReentrantLock lock = new ReentrantLock();
+
+    private static void pinLock() {
+        lock.lock();
+        try {
+            log("Thread A start");
+            sleep(100);
+            log("Thread A end");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private synchronized static void pin() {
+        log("Thread A start");
+        sleep(100);
+        log("Thread A end");
+    }
+
+    private boolean forever() {
+        return true;
     }
 
     private static void log(String s) {
